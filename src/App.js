e9476f926eed67db4899620fd1527dfe34dc0e86
Anna Kova <m3ta007@gmail.com>
2020-07-04 20:36:26 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import PhotoManager from './components/PhotoManager'
+import dataImage from './img/carl-nenzen-loven-igKjieyjcko-unsplash.jpg'
 
 function App() {
+  const prevData = [
+    {
+      src: dataImage,
+      alt:
+        'White road bicycle against a teal wall in the sunset. Photo by Carl Nenzen Loven on Unsplash',
+    },
+    {
+      src:
+        'https://images.unsplash.com/photo-1523740856324-f2ce89135981?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1098&q=80',
+      alt:
+        'Red rusty cruiser bicycle with a basket against a yellow wall. Photo by Chris Barbalis on Unsplash',
+    },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PhotoManager data={prevData} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/PhotoManager.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function PhotoManager(props) {
+  // Load existent data
+  const { data } = props
+
+  console.log(data)
+
+  // Fallback path handling:
+  // function extractFilename(path) {
+  //   if (path.substr(0, 12) == 'C:\\fakepath\\') return path.substr(12) // modern browser
+  //   var x
+  //   x = path.lastIndexOf('/')
+  //   if (x >= 0)
+  //     // Unix-based path
+  //     return path.substr(x + 1)
+  //   x = path.lastIndexOf('\\')
+  //   if (x >= 0)
+  //     // Windows-based path
+  //     return path.substr(x + 1)
+  //   return path // just the file name
+  // }
+
+  // Get DataUrl from uploaded file(s)
+  const fileToDataUrl = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader()
+
+      fileReader.addEventListener('load', (evt) => {
+        resolve(evt.currentTarget.result)
+      })
+
+      fileReader.addEventListener('error', (evt) => {
+        reject(new Error(evt.currentTarget.error))
+      })
+
+      fileReader.readAsDataURL(file)
+    })
+  }
+
+  const handleSelect = async (evt) => {
+    const files = [...evt.target.files]
+    const urls = await Promise.all(files.map((o) => fileToDataUrl(o)))
+    // array with dataUrl to be used as a value for src of img tags
+  }
+
+  const handleRemove = () => {
+    console.log('Handle me!')
+  }
+
+  return (
+    <div className="container">
+      <label htmlFor="photo-input" className="photo-input-label">
+        <h2>Select a photo</h2>
+      </label>
+      {/* Add multiple? https://html.spec.whatwg.org/multipage/input.html#file-upload-state-(type=file) */}
+      <input
+        type="file"
+        name="photo"
+        accept="image/*"
+        // onChange={extractFilename(this.value)}
+        onChange={handleSelect}
+        id="photo-input"
+        className="photo-input"
+      />
+      <p>
+        The photo you picked is: <span id="photo-name">file-name-here.img</span>
+      </p>
+      {/* static images: */}
+      <div className="photo-container">
+        <div className="photo-thumbnail">
+          <img src={data[0].src} alt={data[0].alt} className="thumbnail" />
+          <button className="remove-btn" onClick={() => handleRemove()}>
+            <i className="material-icons" role="presentation">
+              clear
+            </i>
+            <span className="sr-only">Remove {data[0].alt}</span>
+          </button>
+        </div>
+        <div className="photo-thumbnail">
+          <img src={data[1].src} alt={data[1].alt} className="thumbnail" />
+          <button className="remove-btn" onClick={() => handleRemove()}>
+            <i className="material-icons" role="presentation">
+              clear
+            </i>
+            <span className="sr-only">Remove {data[1].alt}</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+PhotoManager.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string,
+      alt: PropTypes.string,
+    })
+  ),
+}
+
+export default PhotoManager
